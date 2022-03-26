85d043fd027d2d5e36be060b2f79a21863430288
yolande <yolande.hattingh@gmail.com>
2022-03-26 12:43:01 +0200

    Maven and xml

--- MyFirstMavenProject/src/test/java/loans/HomeLoan1.java
+++ MyFirstMavenProject/src/test/java/loans/HomeLoan1.java
@@ -1,8 +1,14 @@
 package loans;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class HomeLoan1 {
+	
+	@BeforeClass
+	public void BeforeClassTest() {
+		System.out.println("Clear db - testing BeforeClass");
+	}
 	
 	@Test
 	public void homeLoan1() {
--- MyFirstMavenProject/src/test/java/loans/HomeLoan2.java
+++ MyFirstMavenProject/src/test/java/loans/HomeLoan2.java
@@ -1,8 +1,26 @@
 package loans;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class HomeLoan2 {
+	
+	@BeforeTest
+	public void cleanDBBeforeTest() {
+		System.out.println("Clear db - testing BeforeTest");
+	}
+	
+	@AfterTest
+	public void cleanDBAfterTest() {
+		System.out.println("Clear db - testing AfterTest");
+	}
+	
+	@BeforeMethod
+	public void cleanDBBeforeMethod() {
+		System.out.println("Clear db - testing BeforeMethod");
+	}
 	
 	@Test
 	public void homeLoan3() {
